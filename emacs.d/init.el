33b2a716f759e81594a75091f6ca3414c789f144
Evan Martin <martine@danga.com>
2009-10-24 09:08:57 -0700

    viper mode

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -163,3 +163,11 @@ See also `newline-and-indent'."
 
 (setq browse-url-browser-function 'browse-url-generic
       browse-url-generic-program "google-chrome")
+
+(setq viper-mode t)                ; enable Viper at load time
+(setq viper-ex-style-editing nil)  ; can backspace past start of insert / line
+(require 'viper)                   ; load Viper
+(setq vimpulse-experimental nil)   ; don't load bleeding edge code (see 6. installation instruction)
+(require 'vimpulse)                ; load Vimpulse
+(setq woman-use-own-frame nil)     ; don't create new frame for manpages
+(setq woman-use-topic-at-point t)  ; don't prompt upon K key (manpage display)

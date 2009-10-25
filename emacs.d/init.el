1d130dfde6b3b8fa9d452e8f5feb83d1196a1b79
Evan Martin <martine@danga.com>
2009-10-25 12:46:44 -0700

    drop vimpulse; didn't work right

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -167,7 +167,3 @@ See also `newline-and-indent'."
 (setq viper-mode t)                ; enable Viper at load time
 (setq viper-ex-style-editing nil)  ; can backspace past start of insert / line
 (require 'viper)                   ; load Viper
-(setq vimpulse-experimental nil)   ; don't load bleeding edge code (see 6. installation instruction)
-(require 'vimpulse)                ; load Vimpulse
-(setq woman-use-own-frame nil)     ; don't create new frame for manpages
-(setq woman-use-topic-at-point t)  ; don't prompt upon K key (manpage display)

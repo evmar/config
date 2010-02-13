b163cadeac6ef45441d65dc21c8cb08e176073d8
Evan Martin <martine@danga.com>
2010-02-13 18:44:16 +0100

    pandoc for markdown

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -56,11 +56,12 @@
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
  '(column-number-mode t)
+ '(haskell-program-name "ghci")
+ '(markdown-command "pandoc -f markdown")
  '(org-agenda-files (quote ("~/everything.org")))
  '(show-paren-mode t)
  '(tool-bar-mode nil)
- '(uniquify-buffer-name-style (quote forward))
- '(haskell-program-name "ghci"))
+ '(uniquify-buffer-name-style (quote forward)))
 (custom-set-faces
   ;; custom-set-faces was added by Custom.
   ;; If you edit it by hand, you could mess it up, so be careful.

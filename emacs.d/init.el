ed59d048c5085dc47a6109506604c4040f47fd22
Evan Martin <martine@danga.com>
2010-03-01 14:06:50 +0100

    vala

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -177,3 +177,9 @@ See also `newline-and-indent'."
 
 (setq browse-url-browser-function 'browse-url-generic
       browse-url-generic-program "google-chrome")
+
+(autoload 'vala-mode "vala-mode" "Major mode for editing Vala code." t)
+(add-to-list 'auto-mode-alist '("\\.vala$" . vala-mode))
+(add-to-list 'auto-mode-alist '("\\.vapi$" . vala-mode))
+(add-to-list 'file-coding-system-alist '("\\.vala$" . utf-8))
+(add-to-list 'file-coding-system-alist '("\\.vapi$" . utf-8))

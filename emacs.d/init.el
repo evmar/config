c360c2c7164fdd4d94130fc35da7972eca8ceb1d
Evan Martin <martine@danga.com>
2012-08-19 15:03:12 -0700

    drop open-line, evil does this

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -148,34 +148,6 @@
 (add-to-list 'auto-mode-alist '("\\.scons$" . python-mode))
 ; vi-like keybindings
 
-(defvar newline-and-indent nil
-  "Modify the behavior of the open-*-line functions to cause them
-to autoindent.")
-
-(defun open-previous-line (arg)
-  "Open a new line before the current one.
-
-See also `newline-and-indent'."
-  (interactive "p")
-  (beginning-of-line)
-  (open-line arg)
-  (when newline-and-indent
-    (indent-according-to-mode)))
-
-(defun open-next-line (arg)
-  "Move to the next line and then opens a line.
-
-See also `newline-and-indent'."
-  (interactive "p")
-  (end-of-line)
-  (open-line arg)
-  (forward-line 1)
-  (when newline-and-indent
-    (indent-according-to-mode)))
-
-(global-set-key (kbd "C-o") 'open-next-line)
-(global-set-key (kbd "M-o") 'open-previous-line)
-
 (global-set-key (kbd "M-`") 'next-error)
 
 (global-set-key [f7] (lambda ()

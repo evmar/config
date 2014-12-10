1599712cec9bf7d5f89ffcbfa034c8480b0afb51
Evan Martin <martine@danga.com>
2014-12-10 08:11:48 -0800

    new emacs version

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -58,10 +58,10 @@
 (fset 'yes-or-no-p 'y-or-n-p)
 
 (custom-set-variables
-  ;; custom-set-variables was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(coffee-tab-width 2)
  '(column-number-mode t)
  '(erc-hide-list (quote ("JOIN" "PART" "QUIT")))
@@ -79,13 +79,14 @@
  '(safe-local-variable-values (quote ((c-offsets-alist (innamespace . 0)))))
  '(show-paren-mode t)
  '(tool-bar-mode nil)
- '(uniquify-buffer-name-style (quote forward))
- '(vc-handled-backends nil))
+ '(uniquify-buffer-name-style (quote forward) nil (uniquify))
+ '(vc-handled-backends nil)
+ '(visible-bell t))
 (custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(whitespace-line ((t (:underline "red")))))
 
 ; evil mode
